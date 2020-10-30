05f434f0bdf7b677b033634dbf407c0aed5985e2
Owen Medina <owensmedi@gmail.com>
2020-10-30 11:37:09 -0700

    Initial Commit

---
+++ Coin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin
+//
+//  Created by Owen Medina on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
